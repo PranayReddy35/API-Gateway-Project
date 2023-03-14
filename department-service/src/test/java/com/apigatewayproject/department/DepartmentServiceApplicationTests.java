42b052d2d2232ce3cb17a2b10c6ea2e03f00bc3e
PranayReddy35 <94630894+PranayReddy35@users.noreply.github.com>
2023-03-14 17:35:23 -0400

    Add files via upload

---
+++ department-service/src/test/java/com/apigatewayproject/department/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apigatewayproject.department;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
